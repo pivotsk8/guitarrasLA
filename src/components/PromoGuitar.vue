b36ac3cf1ba344a97fe4be930ad38e7f1d265866
piiivot <the_pivotsk8@hotmail.com>
2023-06-16 11:13:20 -0500

    feat: se separaron responsabilidades en componente

---
+++ src/components/PromoGuitar.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    guitarraPromo: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <div class="row mt-5">
+        <div class="col-md-6 text-center text-md-start pt-5">
+            <h1 class="display-2 fw-bold">Modelo {{ guitarraPromo.nombre }}</h1>
+            <p class="mt-5 fs-5 text-white">{{ guitarraPromo.descripcion }}</p>
+            <p class="text-primary fs-1 fw-black">${{ guitarraPromo.precio }}</p>
+            <button type="button" class="btn fs-4 bg-primary text-white py-2 px-5">Agregar al Carrito</button>
+        </div>
+    </div>
+</template>
+
+
---
+++ src/components/TableCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+    producto: {
+        type: Object,
+        required: true
+    }
+})
+
+defineEmits([
+    'decrementar-cantidad',
+    'incrementar-cantidad'
+])
+</script>
+<template>
+    <td>
+        <img class="img-fluid" :src="'/img/' + 'guitarra_02' + '.jpg'" :alt="'producto.nombre' + 'imagen guitarra'">
+    </td>
+    <td>{{ producto.nombre }}</td>
+    <td class="fw-bold">
+        {{ producto.precio }}
+    </td>
+    <td class="flex align-items-start gap-4">
+        <button type="button" class="btn btn-dark" @click="$emit('decrementar-cantidad', producto.id)">
+            -
+        </button>
+
+        {{ producto.cantidad }}
+
+        <button type="button" class="btn btn-dark" @click="$emit('incrementar-cantidad', producto.id)">
+            +
+        </button>
+    </td>
+    <td>
+        <button class="btn btn-danger" type="button">
+            X
+        </button>
+    </td>
+</template>
